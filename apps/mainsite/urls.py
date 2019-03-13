5be5b9b6493e2c1faa1ba35d86074da23ad26364
Nate Otto <nate@ottonomy.net>
2019-03-12 17:03:46 -0700

    Update url paths to avoid launch warnings

--- apps/mainsite/urls.py
+++ apps/mainsite/urls.py
@@ -12,7 +12,7 @@ from django.views.generic.base import RedirectView, TemplateView
 
 from mainsite.views import SitewideActionFormView, LoginAndObtainAuthToken, RedirectToUiLogin, DocsAuthorizeRedirect
 from mainsite.views import info_view, email_unsubscribe, AppleAppSiteAssociation, error404, error500
-
+from pathway.api import PathwayList
 
 urlpatterns = [
     # Backup URLs in case the server isn't serving these directly
@@ -77,6 +77,7 @@ urlpatterns = [
 
     # NOTE: pathway and recipient were written and deployed for beta testing at /v2/ before /v2/ was formalized
     # they do not conform to new /v2/ conventions,  they need to appear before /v2/ to not collide
+    url(r'^v2/issuers/(?P<issuer_slug>[^/]+)/pathways$', PathwayList.as_view(), name='pathway_list'),
     url(r'^v2/issuers/(?P<issuer_slug>[^/]+)/pathways/', include('pathway.api_urls'), kwargs={'version': 'v1'}),
 
     # recipient was refactored to /v2/, but for now keep the old "v1" API registered at /v2/issuers/<issuer_slug/recipient-groups
