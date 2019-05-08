04a0ad926cfbc115125e82dfe8dba633ce2d6b84
Nathan Nichols <nnichols@concentricsky.com>
2019-05-08 12:11:34 -0700

    Unconfigured blacklist should not raise exception

--- apps/mainsite/blacklist.py
+++ apps/mainsite/blacklist.py
@@ -27,44 +27,42 @@ def api_submit_recipient_id(id_type, recipient_id):
         return None
 
 
-def api_query_recipient_id(id_type, recipient_id):
-    blacklist_api_key = getattr(settings, 'BADGR_BLACKLIST_API_KEY', None)
-    blacklist_query_endpoint = getattr(settings, 'BADGR_BLACKLIST_QUERY_ENDPOINT', None)
-    if blacklist_api_key and blacklist_query_endpoint:
-        recipient_id_hash = generate_hash(id_type, recipient_id)
+def api_query_recipient_id(id_type, recipient_id, blacklist_query_endpoint, blacklist_api_key):
+    recipient_id_hash = generate_hash(id_type, recipient_id)
+    request_query = "{endpoint}?id={recipient_id_hash}".format(
+        endpoint=blacklist_query_endpoint,
+        recipient_id_hash=recipient_id_hash)
 
-        request_query = "{endpoint}?id={recipient_id_hash}".format(
-            endpoint=blacklist_query_endpoint,
-            recipient_id_hash=recipient_id_hash)
-
-        try:
-            response = requests.get(request_query, headers={
-                "Authorization": "BEARER {api_key}".format(
-                    api_key=blacklist_api_key
-                ),
-            })
-        except ConnectionError:
-            return None
-
-        return response
-    else:
+    try:
+        response = requests.get(request_query, headers={
+            "Authorization": "BEARER {api_key}".format(
+                api_key=blacklist_api_key
+            ),
+        })
+    except ConnectionError:
         return None
+
+    return response
 
 
 def api_query_is_in_blacklist(id_type, recipient_id):
-    response = api_query_recipient_id(id_type, recipient_id)
+    blacklist_api_key = getattr(settings, 'BADGR_BLACKLIST_API_KEY', None)
+    blacklist_query_endpoint = getattr(settings, 'BADGR_BLACKLIST_QUERY_ENDPOINT', None)
+    if blacklist_query_endpoint and blacklist_api_key:
+        response = api_query_recipient_id(id_type, recipient_id, blacklist_query_endpoint, blacklist_api_key)
 
-    if response and response.status_code == 200:
-        query = response.json()
-        if len(query) > 0:
-            return True
-        else:
-            return False
+        if response and response.status_code == 200:
+            query = response.json()
+            if len(query) > 0:
+                return True
+            else:
+                return False
 
-    if response is None:
-        raise Exception("Blacklist failed to respond")
+        if response is None:
+            raise Exception("Blacklist failed to respond")
 
-    return None
+        return False
+    return False
 
 
 def generate_hash(id_type, id_value):
