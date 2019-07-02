09bd4869f17dfd7bf6061a6fe9c9b982687b843b
Nathan Nichols <nnichols@concentricsky.com>
2019-07-01 18:11:14 -0700

    Add cached metadata to avoid additional network request

--- apps/badgrsocialauth/models.py
+++ apps/badgrsocialauth/models.py
@@ -3,6 +3,7 @@ from badgeuser.models import BadgeUser
 
 class Saml2Configuration(models.Model):
     metadata_conf_url = models.URLField(verbose_name="Metadata Configuration URL", help_text="The URL for the XML configuration for SAML2 flows. Get this from the Identity Provider Application.")
+    cached_metadata = models.TextField(default='', blank=True, help_text="If the XML is provided here we avoid making a network request to the metadata_conf_url.")
     slug = models.CharField(max_length=32, unique=True, help_text="This slug must be prefixed with saml2.")
 
     def __str__(self):
