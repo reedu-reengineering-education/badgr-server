88d8887260cb828610bc866e8cbf658f76eaab7f
ipossconcentricsky <iposs@concentricsky.com>
2020-05-19 11:18:39 +0200

    BP-2393 In order to increase awareness about cascading model deletion, the on_delete argument of ForeignKey and OneToOneField will be required in Django 2.0

--- apps/pathway/migrations/0001_initial.py
+++ apps/pathway/migrations/0001_initial.py
@@ -64,7 +64,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='pathway',
             name='root_element',
-            field=models.OneToOneField(related_name='toplevel_pathway', null=True, to='pathway.PathwayElement'),
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='toplevel_pathway', null=True, to='pathway.PathwayElement'),
             preserve_default=True,
         ),
     ]
