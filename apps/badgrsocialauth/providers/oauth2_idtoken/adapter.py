eff65f6c737103b7b721f66d0d065fa82f5bd4f9
Nate Otto <notto@concentricsky.com>
2020-05-21 19:30:37 -0700

    Update IdTokenProvider to allow configurable response_type needed to specify id_token response

--- apps/badgrsocialauth/providers/oauth2_idtoken/adapter.py
+++ apps/badgrsocialauth/providers/oauth2_idtoken/adapter.py
@@ -15,6 +15,7 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client, OAuth2Er
 
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import timezone
+from django.utils.http import urlencode
 
 from socialauth.providers.log_configuration import debug_requests
 from .provider import IdTokenProvider
@@ -35,6 +36,7 @@ class IdTokenOAuth2Adapter(OAuth2Adapter):
             self.authorize_url = app_settings.PROVIDERS[self.provider_id]['authorize_url']
             self.jwks_url = app_settings.PROVIDERS[self.provider_id]['jwks_url']
             self.intended_aud = app_settings.PROVIDERS[self.provider_id]['aud']
+            self.response_type = app_settings.PROVIDERS[self.provider_id].get('response_type', 'code')
         except KeyError as e:
             raise ImproperlyConfigured(self.provider_id)
 
@@ -76,7 +78,7 @@ class IdTokenOAuth2Adapter(OAuth2Adapter):
         # token has already been verified, so we can use unverified data here
         unverified_header, unverified_claims = jwt.process_jwt(token.token)
         extra_data = unverified_claims
-        logger.debug('IHRPSSO | IHRPOAuth2Adapter:complete_login()'
+        logger.debug('IdToken | IdTokenOAuth2Adapter:complete_login()'
                      'token claims |  %s' % str(extra_data))
 
         return self.get_provider().sociallogin_from_response(request,
@@ -84,6 +86,22 @@ class IdTokenOAuth2Adapter(OAuth2Adapter):
 
 
 class IdTokenOAuth2Client(OAuth2Client):
+    def __init__(self, *args, **kwargs):
+        super(IdTokenOAuth2Client, self).__init__(*args, **kwargs)
+        self.response_type = kwargs.get('response_type', 'code')
+
+    def get_redirect_url(self, authorization_url, extra_params):
+        params = {
+            'client_id': self.consumer_key,
+            'redirect_uri': self.callback_url,
+            'scope': self.scope,
+            'response_type': self.response_type
+        }
+        if self.state:
+            params['state'] = self.state
+        params.update(extra_params)
+        return '%s?%s' % (authorization_url, urlencode(params))
+
     def get_access_token(self, code):
         data = {
             'redirect_uri': self.callback_url,
@@ -127,24 +145,28 @@ class IdTokenOAuth2Client(OAuth2Client):
         return access_token
 
 
-class IHRPOAuth2CallbackView(OAuth2CallbackView):
+class IdTokenOAuth2CallbackView(OAuth2CallbackView):
     def get_client(self, request, app):
         callback_url = self.adapter.get_callback_url(request, app)
         provider = self.adapter.get_provider()
+        response_type = getattr(provider, 'response_type', 'code')
         scope = provider.get_scope(request)
-        client = IdTokenOAuth2Client(self.request, app.client_id, app.secret,
-                              self.adapter.access_token_method,
-                              self.adapter.access_token_url,
-                              callback_url,
-                              scope,
-                              scope_delimiter=self.adapter.scope_delimiter,
-                              headers=self.adapter.headers,
-                              basic_auth=self.adapter.basic_auth)
+        client = IdTokenOAuth2Client(
+            request, app.client_id, app.secret,
+            request.access_token_method,
+            self.adapter.access_token_url,
+            callback_url,
+            scope,
+            scope_delimiter=self.adapter.scope_delimiter,
+            headers=self.adapter.headers,
+            basic_auth=self.adapter.basic_auth,
+            response_type=response_type
+        )
         return client
 
 
 oauth2_login = OAuth2LoginView.adapter_view(IdTokenOAuth2Adapter)
-base_oauth2_callback = IHRPOAuth2CallbackView.adapter_view(IdTokenOAuth2Adapter)
+base_oauth2_callback = IdTokenOAuth2CallbackView.adapter_view(IdTokenOAuth2Adapter)
 def oauth2_callback(*args, **kwargs):
     with debug_requests():
         return base_oauth2_callback(*args, **kwargs)
