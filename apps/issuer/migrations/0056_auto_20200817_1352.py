1c52f1d49744881664686fb00706ff374ff43272
Nate Otto <notto@concentricsky.com>
2020-08-31 18:29:59 -0700

    update migration dependency tree to avoid conflict

--- apps/issuer/migrations/0056_auto_20200817_1352.py
+++ apps/issuer/migrations/0056_auto_20200817_1352.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('issuer', '0054_auto_20200724_1317'),
+        ('issuer', '0055_auto_20200817_1538'),
     ]
 
     operations = [
