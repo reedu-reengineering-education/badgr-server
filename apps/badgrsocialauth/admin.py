55ff55b09804608b5c1006aed4a3dca69ff327c4
Nate Otto <nate@ottonomy.net>
2020-11-19 19:40:50 -0800

    Customize Saml2ConfigurationAdmin to have clearer error validation for custom_settings

--- apps/badgrsocialauth/admin.py
+++ apps/badgrsocialauth/admin.py
@@ -1,13 +1,37 @@
+import json
+
 from django.contrib.admin import ModelAdmin
+from django import forms
 from mainsite.admin import badgr_admin
 from .models import Saml2Configuration, Saml2Account
 
 
+class Saml2ConfigurationAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Saml2Configuration
+        fields = ('metadata_conf_url', 'cached_metadata', 'slug', 'use_signed_authn_request',
+                  'custom_settings')
+
+
+    def clean(self):
+        custom_settings = self.cleaned_data.get('custom_settings')
+        try:
+            data = json.loads(custom_settings)
+            if not isinstance(data, dict):
+                raise ValueError()
+        except (TypeError, ValueError,):
+            raise forms.ValidationError(
+                "custom_settings must be a valid JSON. email, first_name, and last_name keys are valid."
+            )
+
+        return self.cleaned_data
+
+
 class Saml2ConfigurationModelAdmin(ModelAdmin):
-    model = Saml2Configuration
+    form = Saml2ConfigurationAdminForm
     readonly_fields = ('acs_url', 'sp_metadata_url')
-    fields = ('metadata_conf_url', 'cached_metadata', 'slug', 'acs_url', 'sp_metadata_url', 'use_signed_authn_request',
-              'custom_settings')
+
 badgr_admin.register(Saml2Configuration, Saml2ConfigurationModelAdmin)
 
 
