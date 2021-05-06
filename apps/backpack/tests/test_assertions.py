ce3ea01676539bc3323c0dc3b1e3a9c9bbb1891d
Louis Byers <louis@concentricsky.com>
2021-05-06 13:47:52 -0700

    BP-6100 - changing response code for invalid image test for v1 backpack upload api

--- apps/backpack/tests/test_assertions.py
+++ apps/backpack/tests/test_assertions.py
@@ -808,7 +808,7 @@ class TestBadgeUploads(BadgrTestCase):
                 '/v1/earner/badges', post_input
             )
 
-        self.assertEqual(response.status_code, 415)
+        self.assertEqual(response.status_code, 400)
 
         get_response = self.client.get('/v1/earner/badges')
         self.assertEqual(get_response.status_code, 200)
